0e50fb18d6ab08b715f1a0bbec85d5ac56626b47
Balthazar Gronon <git@balthazar.dev>
2023-05-01 13:14:01 -0600

    feat: Update delegation module & codec types for amino support

--- x/delegation/types/codec.go
+++ x/delegation/types/codec.go
@@ -3,20 +3,33 @@ package types
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
+	cryptocodec "github.com/cosmos/cosmos-sdk/crypto/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-func RegisterCodec(_ *codec.LegacyAmino) {}
+func RegisterLegacyAminoCodec(cdc *codec.LegacyAmino) {
+	cdc.RegisterConcrete(&MsgDelegate{}, "kyve/Delegate", nil)
+	cdc.RegisterConcrete(&MsgUndelegate{}, "kyve/Undelegate", nil)
+	cdc.RegisterConcrete(&MsgRedelegate{}, "kyve/Redelegate", nil)
+	cdc.RegisterConcrete(&MsgWithdrawRewards{}, "kyve/WithdrawRewards", nil)
+	cdc.RegisterConcrete(&MsgUpdateParams{}, "kyve/UpdateParams", nil)
+}
 
 func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 	registry.RegisterImplementations((*sdk.Msg)(nil), &MsgDelegate{})
-	registry.RegisterImplementations((*sdk.Msg)(nil), &MsgWithdrawRewards{})
 	registry.RegisterImplementations((*sdk.Msg)(nil), &MsgUndelegate{})
 	registry.RegisterImplementations((*sdk.Msg)(nil), &MsgRedelegate{})
+	registry.RegisterImplementations((*sdk.Msg)(nil), &MsgWithdrawRewards{})
 	registry.RegisterImplementations((*sdk.Msg)(nil), &MsgUpdateParams{})
 }
 
 var (
-	Amino     = codec.NewLegacyAmino()
-	ModuleCdc = codec.NewProtoCodec(cdctypes.NewInterfaceRegistry())
+	amino     = codec.NewLegacyAmino()
+	ModuleCdc = codec.NewAminoCodec(amino)
 )
+
+func init() {
+	RegisterLegacyAminoCodec(amino)
+	cryptocodec.RegisterCrypto(amino)
+	sdk.RegisterLegacyAminoCodec(amino)
+}
